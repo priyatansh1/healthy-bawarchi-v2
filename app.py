f720844277f9a2a996cbf9ba757303f2973b84bb
priyatansh1 <priyatansh@gmail.com>
2026-05-05 14:39:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
     page_title="Healthy Bawarchi — Smart Recipe Generator",
     page_icon="🥗",
     layout="centered",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 
